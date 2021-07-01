c2265418b9e45ad45525b20249e1b7b9dfd39bf3
Tozuka Yuji <yuji_tozuka@ydktechs.co.jp>
2021-07-01 16:47:04 +0900

    fin

--- mysite/settings.py
+++ mysite/settings.py
@@ -131,9 +131,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = '/usr/share/nginx/html/static'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
-)
+STATICFILES_DIRS = '/usr/share/nginx/html/static'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
